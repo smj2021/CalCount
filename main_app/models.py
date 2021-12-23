2a0ce6ac286bceae03f10007a50cbf9e6daed1b2
smj2021 <mjschwartz515@gmail.com>
2021-12-23 14:33:44 -0500

    start Fooditem model

--- main_app/models.py
+++ main_app/models.py
@@ -2,15 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    
+
     def __str__(self):
         return str(self.name)
-    
+
+
 class Category(models.Model):
     options = (
         ('breakfast', 'breakfast'),
@@ -19,6 +22,12 @@ class Category(models.Model):
         ('snacks', 'snacks'),
     )
     name = models.CharField(max_length=50, choices=options)
-    
+
     def __str__(self):
         return self.name
+
+
+class Fooditem(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ManyToManyField(Category)
+    
