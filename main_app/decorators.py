5460612805b3b1d7cc25633149514284a8c5238c
smj2021 <mjschwartz515@gmail.com>
2021-12-23 15:04:04 -0500

    user auth function

--- main_app/decorators.py
+++ main_app/decorators.py
@@ -1,1 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
+
+def unauthorized_user(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
