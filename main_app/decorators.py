4bfc12066c058e90565c82d19af160998fed023c
smj2021 <mjschwartz515@gmail.com>
2021-12-23 15:16:36 -0500

    user authorization function complete

--- main_app/decorators.py
+++ main_app/decorators.py
@@ -16,4 +16,11 @@ def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
             group=None
-            
+            if request.user.groups.exists():
+                group=request.user.groups.all() [0].name
+            if group in allowed_roles:
+                return view_func(request, *args, **kwargs)
+            else:
+                return HttpResponse("<h1>You are not authorized to acces this page, chump!</h1>")
+        return wrapper_func
+    return decorator
