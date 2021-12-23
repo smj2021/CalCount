30f267ffc6156830a44dd551fd3408f481b4e028
smj2021 <mjschwartz515@gmail.com>
2021-12-23 15:09:56 -0500

    begin user authorization function

--- main_app/decorators.py
+++ main_app/decorators.py
@@ -9,6 +9,11 @@ def unauthorized_user(view_func):
         if request.user.is_authenticated:
             return redirect('home')
         else:
-            return view_func(request, *args, *kwargs)
+            return view_func(request, *args, **kwargs)
     return wrapper_func
 
+def allowed_users(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwargs):
+            group=None
+            
