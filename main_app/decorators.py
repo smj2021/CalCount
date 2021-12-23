b691d7df1916b0eb0678cf0941f80355f204c142
smj2021 <mjschwartz515@gmail.com>
2021-12-23 15:29:59 -0500

    admin auth complete

--- main_app/decorators.py
+++ main_app/decorators.py
@@ -12,12 +12,13 @@ def unauthorized_user(view_func):
             return view_func(request, *args, **kwargs)
     return wrapper_func
 
+
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
-            group=None
+            group = None
             if request.user.groups.exists():
-                group=request.user.groups.all() [0].name
+                group = request.user.groups.all()[0].name
             if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
             else:
@@ -25,7 +26,14 @@ def allowed_users(allowed_roles=[]):
         return wrapper_func
     return decorator
 
+
 def admin_only(view_func):
     def wrapper_func(request, *args, **kwargs):
-        group=None
-        
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'user':
+            return redirect('userPage')
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+    return wrapper_func
