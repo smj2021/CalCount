a7b0fbbdafbe4b0fb972f44c430ebcb01e3386cc
smj2021 <mjschwartz515@gmail.com>
2021-12-27 17:08:11 -0500

    add fooditem view

--- main_app/views.py
+++ main_app/views.py
@@ -13,7 +13,7 @@ from .filters import fooditemFilter
 # Create your views here.
 
 
-@login_required
+@login_required(login_url='login')
 @admin_only
 def home(request):
     breakfast = Category.objects.filter(name='breakfast')[
@@ -23,9 +23,34 @@ def home(request):
     snacks = Category.objects.filter(name='snacks')[0].fooditem_set.all()[:5]
     customers = Customer.objects.all()
     context = {'breakfast': breakfast,
-                'lunch': lunch,
-                'dinner': dinner,
-                'snacks': snacks,
-                'customers': customers,
+               'lunch': lunch,
+               'dinner': dinner,
+               'snacks': snacks,
+               'customers': customers,
                }
+    return render(request, 'main.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
+def fooditem(request):
+    breakfast = Category.objects.filter(name='breakfast')[0].fooditem_set.all()
+    bcnt = breakfast.count()
+    lunch = Category.objects.filter(name='lunch')[0].fooditem_set.all()
+    lcnt = lunch.count()
+    dinner = Category.objects.filter(name='dinner')[0].fooditem_set.all()
+    dcnt = dinner.count()
+    snacks = Category.objects.filter(name='snacks')[0].fooditem_set.all()
+    scnt = snacks.count()
+    context = {'breakfast': breakfast,
+               'bcnt': bcnt,
+               'lcnt': lcnt,
+               'scnt': scnt,
+               'dcnt': dcnt,
+               'lunch': lunch,
+               'dinner': dinner,
+               'snacks': snacks,
+               }
+    return render(request, 'fooditem.html', context)
+
 
