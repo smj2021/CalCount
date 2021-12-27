2ea29af382a18ad5a66088143253031e89d2ff7e
smj2021 <mjschwartz515@gmail.com>
2021-12-27 16:19:12 -0500

    add admin model encapsulation

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class foodAdmin(admin.ModelAdmin):
+    class Meta:
+        model=Fooditem
+    list_display=['name']
+    list_filter=['name']
+    
