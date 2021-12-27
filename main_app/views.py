ae33a763238c3fbd065104846c38d0892e8be088
smj2021 <mjschwartz515@gmail.com>
2021-12-27 16:48:34 -0500

    add home view

--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,21 @@ from django.contrib.auth.models import Group
 from .filters import fooditemFilter
 
 # Create your views here.
+
+
+@login_required
+@admin_only
+def home(request):
+    breakfast = Category.objects.filter(name='breakfast')[
+        0].fooditem_set.all()[:5]
+    lunch = Category.objects.filter(name='lunch')[0].fooditem_set.all()[:5]
+    dinner = Category.objects.filter(name='dinner')[0].fooditem_set.all()[:5]
+    snacks = Category.objects.filter(name='snacks')[0].fooditem_set.all()[:5]
+    customers = Customer.objects.all()
+    context = {'breakfast': breakfast,
+                'lunch': lunch,
+                'dinner': dinner,
+                'snacks': snacks,
+                'customers': customers,
+               }
+
