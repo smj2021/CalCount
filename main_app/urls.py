f7457a514a3fc0ecc8fc6b9500645d765114a5b6
smj2021 <mjschwartz515@gmail.com>
2021-12-27 16:32:41 -0500

    add home, user, fooditem, createfood, register routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,3 +2,11 @@ from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('user/', views.userPage, name='userPage'), #userPage
+    path('product/', views.fooditem, name='fooditem'),
+    path('createfooditem/', views.createfooditem, name='createfooditem'),
+    path('register/', views.loginPage, name='login'),
+    
+]
