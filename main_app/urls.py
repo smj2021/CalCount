28f74860cab5450f4ca5dfcb79f02be32290cc6e
smj2021 <mjschwartz515@gmail.com>
2021-12-27 16:40:17 -0500

    password reset confirmation routes added

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,14 +4,19 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('user/', views.userPage, name='userPage'), #userPage
+    path('user/', views.userPage, name='userPage'),  # userPage
     path('product/', views.fooditem, name='fooditem'),
     path('createfooditem/', views.createfooditem, name='createfooditem'),
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('addFooditem/', views.addFooditem, name='addFooditem'),
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'), #protected view for user pw reset
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    
+    path('reset_password/', auth_views.PasswordResetView.as_view(),
+         name='reset_password'),  # protected view for user pw reset
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
