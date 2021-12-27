de8d1a6c65d8e0b1ea9d155443184f64d009df27
smj2021 <mjschwartz515@gmail.com>
2021-12-27 17:36:35 -0500

    ln 94 corrected funtion grammar

--- main_app/views.py
+++ main_app/views.py
@@ -91,7 +91,7 @@ def loginPage(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-        if user in not None:
+        if user is not None:
             login(request, user)
             return redirect('home')
         else:
