76cce7dedb7179f397a3da92c04d1cfc95076445
smj2021 <mjschwartz515@gmail.com>
2021-12-27 16:37:15 -0500

    add password reset routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('user/', views.userPage, name='userPage'), #userPage
     path('product/', views.fooditem, name='fooditem'),
     path('createfooditem/', views.createfooditem, name='createfooditem'),
-    path('register/', views.loginPage, name='login'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('addFooditem/', views.addFooditem, name='addFooditem'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'), #protected view for user pw reset
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     
 ]
