e0179191a107908a15bc55b5a82ff680f6094a34
smj2021 <mjschwartz515@gmail.com>
2021-12-27 16:43:31 -0500

    add imports to views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from .models import *
+from .forms import *
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .decorators import *
+from django.contrib.auth.models import Group
+from .filters import fooditemFilter
 
 # Create your views here.
